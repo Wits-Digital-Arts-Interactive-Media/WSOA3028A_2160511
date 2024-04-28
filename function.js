24c0f641be1b6a8930fd8559ddc29aa442878f77
KceeM <141727280+KceeM@users.noreply.github.com>
2024-04-28 10:57:05 +0200

    Update function.js

---
+++ function.js
@@ -0,0 +1,37 @@
+const homeBtn = document.getElementById('homeBtn');
+const blogBtn = document.getElementById('blogBtn');
+const portfolioBtn = document.getElementById('portfolioBtn');
+const documentBtn = document.getElementById('documentBtn');
+const contactBtn = document.getElementById('contactBtn');
+const essayBtn = document.getElementById('essayBtn');
+
+
+homeBtn.addEventListener('click', function() {
+    
+    window.location.href = 'index.html';
+});
+
+blogBtn.addEventListener('click', function() {
+    
+    window.location.href = 'blog.html';
+});
+
+portfolioBtn.addEventListener('click', function() {
+    
+    window.location.href = 'portfolio.html';
+});
+
+documentBtn.addEventListener('click', function() {
+    
+    window.location.href = 'document.html';
+});
+
+contactBtn.addEventListener('click', function() {
+    
+    window.location.href = 'contact.html';
+});
+
+essayBtn.addEventListener('click', function() {
+    
+    window.location.href = 'essay.html';
+});
