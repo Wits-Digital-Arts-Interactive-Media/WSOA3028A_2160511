98ef4d4a97260547a2e42d0801721eea593a2941
KceeM <141727280+KceeM@users.noreply.github.com>
2024-06-24 02:42:43 +0200

    eve

--- portfolio.js
+++ portfolio.js
@@ -1,8 +1,16 @@
-function toggleDescription(id) {
+function toggleDescription(id, button) {
     const desc = document.getElementById(id);
     if (desc.style.display === "none" || desc.style.display === "") {
         desc.style.display = "block";
+        button.textContent = "Close";
     } else {
         desc.style.display = "none";
+        button.textContent = "More Info";
     }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const descriptions = document.querySelectorAll('.description');
+    descriptions.forEach(desc => desc.style.display = 'none');
+});
---
+++ transforms.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    AOS.init({
+        duration: 1200, 
+        once: true, 
+    });
+});
